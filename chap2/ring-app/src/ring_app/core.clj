8a69459c3113cfb22cab65e13b49554e88f5c4dd
Minsun Lee <dalzony@gmail.com>
2016-06-14 13:39:49 +0900

    Define the Routes with Compojure - 라우팅 추가

--- chap2/ring-app/src/ring_app/core.clj
+++ chap2/ring-app/src/ring_app/core.clj
@@ -2,7 +2,8 @@
   (:require [ring.adapter.jetty :as jetty]
             [compojure.core :as compojure]
             [ring.util.http-response :as response]
-            [ring.middleware.reload :refer [wrap-reload]]))
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.middleware.format :refer [wrap-restful-format]]))
 
 (defn response-handler [request]
   (response/ok
@@ -12,7 +13,9 @@
 
 (def handler
   (compojure/routes
-   (compojure/GET "/" request response-handler)))
+   (compojure/GET "/" request response-handler)
+   (compojure/GET "/:id" [id] (str "<p>the id is: " id "</p>"))
+   (compojure/POST "/json" [id] (response/ok {:result id}))))
 
 (defn wrap-nocache [handler]
   (fn [request]
@@ -20,9 +23,14 @@
         handler
         (assoc-in [:headers "Pragma"] "no-cache"))))
 
+(defn wrap-formats [handler]
+  (wrap-restful-format
+   handler
+   {:formats [:json-kw :transit-json :transit-msgpack]}))
+
 (defn -main []
   (jetty/run-jetty
-   (-> #'handler wrap-nocache wrap-reload)
+   (-> #'handler wrap-nocache wrap-reload wrap-formats)
    {:port 3000
     :join? false}))
 
